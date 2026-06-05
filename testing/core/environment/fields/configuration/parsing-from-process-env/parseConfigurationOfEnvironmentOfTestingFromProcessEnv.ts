ABC-PIEKNEJ-WYMOWY/abc-pieknej-wymowy

chore: Add creating the environment of testing from process.env

testing/core/environment/fields/configuration/parsing-from-process-env/parseConfigurationOfEnvironmentOfTestingFromProcessEnv.ts
@@ -1,0 +1,9 @@
+import type {schemaForProcessEnvOfTesting} from "../../../../schema-for-process-env/schemaForProcessEnvOfTesting.ts";
+import type {ConfigurationOfEnvironmentOfTesting} from "../ConfigurationOfEnvironmentOfTesting.ts";
+import type {z} from "zod";
+export function parseConfigurationOfEnvironmentOfTestingFromProcessEnv(
+	processEnv: z.output<typeof schemaForProcessEnvOfTesting>,
+): ConfigurationOfEnvironmentOfTesting {
+	const configuration: ConfigurationOfEnvironmentOfTesting = {};
+	return configuration;
+}

native-typescript/rfc-9051

testing/core/environment/fields/configuration/parsing-from-process-env/parseConfigurationOfEnvironmentOfTestingFromProcessEnv.ts
@@ -1,0 +1,9 @@
+import type {schemaForProcessEnvOfTesting} from "../../../../schema-for-process-env/index.ts";
+import type {ConfigurationOfEnvironmentOfTesting} from "../ConfigurationOfEnvironmentOfTesting.ts";
+import type {z} from "zod";
+export function parseConfigurationOfEnvironmentOfTestingFromProcessEnv(
+	processEnv: z.output<typeof schemaForProcessEnvOfTesting>,
+): ConfigurationOfEnvironmentOfTesting {
+	const configuration: ConfigurationOfEnvironmentOfTesting = {};
+	return configuration;
+}